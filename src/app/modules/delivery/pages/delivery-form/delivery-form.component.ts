a8dc4aaee7bc163b8929c7dba29d1df8c0676881
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-04-19 14:48:28 -0600

    FEAT / Added validators to delivery form

--- src/app/modules/delivery/pages/delivery-form/delivery-form.component.ts
+++ src/app/modules/delivery/pages/delivery-form/delivery-form.component.ts
@@ -7,41 +7,43 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./delivery-form.component.scss']
 })
 export class DeliveryFormComponent implements OnInit {
-	deliveryForm: FormGroup;
-	currentDate = new Date();
 
+	deliveryForm: FormGroup;
+
+	// * Set min Date on input:date
+	currentDate = new Date();
+	year = this.currentDate.getFullYear();
+	month = String(this.currentDate.getMonth() + 1).padStart(2, '0');
+	day = String(this.currentDate.getDate() + 2).padStart(2, '0');
+	minDate = `${this.year}-${this.month}-${this.day}`;
 
 	constructor() { }
 
 	ngOnInit(): void {
 		this.deliveryForm = new FormGroup({
 			'customerName': new FormControl(null, Validators.required),
-			'customerPhone': new FormControl(null, Validators.required),
-			'deliveryDate': new FormControl(null, Validators.required)
+			'customerPhone': new FormControl(null, [
+				Validators.required,
+				Validators.pattern('[0-9]{10}')
+			]),
+			'deliveryDate': new FormControl(null, Validators.required),
+			'shippingType': new FormControl(null, Validators.required),
+			'customerAddress': new FormControl(null, Validators.required),
+			'postalCode': new FormControl(null, [
+				Validators.required,
+				Validators.pattern(/^[1-9]+[0-9]*$/),
+				Validators.minLength(6)
+			]),
 		})
+	}
 
-
-		const maxDate = this.currentDate.toISOString().slice(0, 10);
-		this.deliveryForm.get('deliveryDate').setValidators([
-			Validators.required,
-			Validators.max(new Date(maxDate).getTime())
-		]);
-
-
-		this.currentDate.setDate(this.currentDate.getDate() + 2);
-
-		const minDate = this.currentDate.toISOString().slice(0, 10);
-		this.deliveryForm.get('deliveryDate').setValidators([
-			Validators.required,
-			Validators.min(new Date(minDate).getTime())
-		])
-
-		this.deliveryForm.get('deliveryDate').updateValueAndValidity();
-
-
+	validateNumericInput(event: any) {
+		const input = event.target as HTMLInputElement;
+		const numericValue = input.value.replace(/[^0-9]/g, '');
+		input.value = numericValue;
 	}
 
 	onSubmit() {
-
+		console.log(this.deliveryForm.value)
 	}
 }
