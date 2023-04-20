a746b83e9fc27edc1cbc8971bd8ef3048d0a3812
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-04-19 19:15:53 -0600

    FEAT / Resume delivery and instructions

---
+++ src/app/modules/delivery/components/instrucctions/instrucctions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instrucctions',
+  templateUrl: './instrucctions.component.html',
+  styleUrls: ['./instrucctions.component.scss']
+})
+export class InstrucctionsComponent {
+
+}
---
+++ src/app/modules/delivery/components/resume-order/resume-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-order',
+  templateUrl: './resume-order.component.html',
+  styleUrls: ['./resume-order.component.scss']
+})
+export class ResumeOrderComponent {
+
+}
