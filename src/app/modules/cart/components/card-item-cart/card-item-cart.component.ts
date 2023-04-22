a42b842c40b485e1f205786faf998d055c0fee9f
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-04-22 10:33:17 -0600

    HOTFIX / Occult delete option card cart product in order-confirmation page

--- src/app/modules/cart/components/card-item-cart/card-item-cart.component.ts
+++ src/app/modules/cart/components/card-item-cart/card-item-cart.component.ts
@@ -1,21 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ICartProduct } from 'src/app/data/interfaces/icartproduct';
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-card-item-cart',
-  templateUrl: './card-item-cart.component.html',
-  styleUrls: ['./card-item-cart.component.scss']
+	selector: 'app-card-item-cart',
+	templateUrl: './card-item-cart.component.html',
+	styleUrls: ['./card-item-cart.component.scss']
 })
 export class CardItemCartComponent {
 
 	@Input() productOrder: ICartProduct;
 	@Input() indexItem: number;
 
-	constructor(private cartService: CartService) { }
-
+	constructor(
+		private cartService: CartService,
+		private router: Router
+	) { }
 
 	onDeleteProduct(index: number) {
 		this.cartService.deleteProduct(index);
+	}
+
+	onHideOptionToDeleteItem(): boolean {
+		return this.router.url === `/delivery/order-confirmation`;
 	}
 }
