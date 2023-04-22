404128fa203942901382021504c085587aa86558
0HungryDeveloper <hungrystudentdev@gmail.com>
2023-04-21 22:02:14 -0600

    FEAT / Go back to catalogue products page

--- src/app/modules/delivery/components/instrucctions/instrucctions.component.ts
+++ src/app/modules/delivery/components/instrucctions/instrucctions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-instrucctions',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class InstrucctionsComponent {
 
+	constructor(private router: Router) { }
+
+	onKeepBuying() {
+		this.router.navigate([`/catalog/products`]);
+	}
 }
